3f8c2721089e729ea28e4b22241d348f7731a7d8
Henrique César <henrique.jose@zup.com.br>
2021-12-07 18:40:49 -0300

    criando exceções de negócio

---
+++ src/main/java/br/com/zup/orquestrador/excecoes/impl/ContaNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package br.com.zup.orquestrador.excecoes.impl;
+
+public class ContaNaoEncontradaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/br/com/zup/orquestrador/excecoes/impl/SaldoInsuficienteException.java
@@ -0,0 +1,7 @@
+package br.com.zup.orquestrador.excecoes.impl;
+
+public class SaldoInsuficienteException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
